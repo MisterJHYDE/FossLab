8589e83fd06b2d14ed4e0136093491eef147b058
stevepchacko <41826421+stevepchacko@users.noreply.github.com>
2018-10-24 19:23:44 +0530

    Update ShellScript2.sh
    i added a comment

--- ShellScript2.sh
+++ ShellScript2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #5. Write shell script to show various system configuration
-
+#hey wassup man im steve!
 echo "OS Version, Release Number, Kernel: ";  uname -a
 echo
 echo "Available Shells: ";  cat /etc/shells
